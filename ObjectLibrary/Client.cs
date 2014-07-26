172d0139d604a91098801bc4cd04a32edd291f49
Jeffroe <jturner@sparkoverflow.com>
2014-07-25 23:24:22 -0400

    stubed out the client object

---
+++ ObjectLibrary/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ObjectLibrary
+{
+    class Client : BaseObject
+    {
+        public virtual string CompassNumber { get; set; }
+        public virtual string SSN { get; set; }
+        public virtual string Suffix { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+
+        public virtual string StateIssuedNumber { get; set; }
+        public virtual string SISNumber { get; set; }
+        public virtual DateTime BirthDate { get; set; }
+        public virtual string BirthLocation { get; set; }
+        //public virtual string Gender { get; set; }
+       
+        //public virtual string Education { get; set; }
+        //public virtual string Phones { get; set; }
+        //public virtual string Addresses { get; set; }
+        //public virtual string Marraige { get; set; }
+        //public virtual string Military { get; set; }
+        
+    }
+}
