ddd8ee21ec505f8f3893a89a63e2ae139728b3fd
JeffroeBodine <jturner@sparkoverflow.com>
2014-07-31 13:57:55 -0400

    updated references to webapi2

--- WebAPI/Controllers/CaseController.cs
+++ WebAPI/Controllers/CaseController.cs
@@ -17,16 +17,13 @@ namespace WebAPI.Controllers
             }
         }
 
-        public HttpResponseMessage Add([FromBody]Case value)
+        public IHttpActionResult Add([FromBody]Case value)
         {
             using (var rb = new RepositoryBase())
             {
                 var id = rb.Add(value);
-                //value.ID = id;
-                var httpResponseMessage = Request.CreateResponse(HttpStatusCode.Created, value);
-                httpResponseMessage.Headers.Location = new Uri(Request.RequestUri, id.ToString());
-       
-                return httpResponseMessage;
+                var uri = new Uri(Request.RequestUri, id.ToString());
+                return Created(uri, value);
             }
         }
     }
