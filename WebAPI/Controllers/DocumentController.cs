096d4a09584a39090b5f4508faea87dd7a47e9bc
JeffroeBodine <jturner@sparkoverflow.com>
2014-09-05 17:57:14 -0400

    changed client and added some prelim cDocument streaming stuff

--- WebAPI/Controllers/DocumentController.cs
+++ WebAPI/Controllers/DocumentController.cs
@@ -1,5 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Web.Http;
 using DMSPlugins.OnBase13;
 using DataAccess;
@@ -23,13 +32,115 @@ namespace WebAPI.Controllers
             return dms.GetDocuments(compassNumber);
         }
 
-        public IHttpActionResult Add([FromBody] CreateDocumentParms parms)
-        {
-            var dms = new OnBase("jturner", "jturner");
-            var id = dms.CreateDocument(parms);
+        //public IHttpActionResult Add([FromBody] CreateDocumentParms parms)
+        //{
+        //    var dms = new OnBase("jturner", "jturner");
+        //    var id = dms.CreateDocument(parms);
 
-            var uri = new Uri(Request.RequestUri, id);
-            return Created(uri, id);
+        //    var uri = new Uri(Request.RequestUri, id);
+        //    return Created(uri, id);
+        //}
+
+        public IHttpActionResult Add(ImageSet model)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendFormat("Received image set {0}: ", model.Name);
+            model.Images.ForEach(i =>
+                sb.AppendFormat("Got image {0} of type {1} and size {2} bytes,",
+                    i.FileName,
+                    i.MimeType,
+                    i.ImageData.Length)
+                );
+
+            var result = sb.ToString();
+            Trace.Write(result);
+
+            return Ok(result);
+
+            return null;
+        }
+
+    }
+
+
+    public class ImageSet
+    {
+        public string Name { get; set; }
+
+        public List<Image> Images { get; set; }
+    }
+
+    public class Image
+    {
+        public string FileName { get; set; }
+
+        public string MimeType { get; set; }
+
+        public byte[] ImageData { get; set; }
+    }
+
+
+    public class ImageSetMediaTypeFormatter : MediaTypeFormatter
+    {
+        public ImageSetMediaTypeFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("multipart/form-data"));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            return type == typeof(ImageSet);
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            return false;
+        }
+
+        public async override Task<object> ReadFromStreamAsync(
+            Type type,
+            Stream readStream,
+            HttpContent content,
+            IFormatterLogger formatterLogger)
+        {
+            var provider = await content.ReadAsMultipartAsync();
+
+            var modelContent = provider.Contents
+                .FirstOrDefault(c => c.Headers.ContentDisposition.Name.NormalizeName() == "imageset");
+
+            var imageSet = await modelContent.ReadAsAsync<ImageSet>();
+
+            var fileContents = provider.Contents
+                .Where(c => c.Headers.ContentDisposition.Name.NormalizeName().Matches(@"image\d+"))
+                .ToList();
+
+            imageSet.Images = new List<Image>();
+            foreach (var fileContent in fileContents)
+            {
+                imageSet.Images.Add(new Image
+                {
+                    ImageData = await fileContent.ReadAsByteArrayAsync(),
+                    MimeType = fileContent.Headers.ContentType.MediaType,
+                    FileName = fileContent.Headers.ContentDisposition.FileName.NormalizeName()
+                });
+            }
+
+            return imageSet;
+
+        }
+    }
+
+    public static class StringExtenstions
+    {
+        public static string NormalizeName(this string text)
+        {
+            return text.Replace("\"", "");
+        }
+
+        public static bool Matches(this string text, string pattern)
+        {
+            return Regex.IsMatch(text, pattern);
         }
     }
 }
--- WebAPIClient/Form1.cs
+++ WebAPIClient/Form1.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using System.Web;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ObjectLibrary;
 
 namespace WebAPIClient
 {
@@ -14,6 +18,95 @@ namespace WebAPIClient
         public Form1()
         {
             InitializeComponent();
+
+            var imageSet = new ImageSet()
+            {
+                Name = "Model",
+                Images = Directory
+                    .EnumerateFiles("../../../../../SampleImages")
+                    .Where(file => new[] { ".jpg", ".png" }.Contains(Path.GetExtension(file)))
+                    .Select(file => new Image
+                    {
+                        FileName = Path.GetFileName(file),
+                        MimeType = MimeMapping.GetMimeMapping(file),
+                        ImageData = File.ReadAllBytes(file)
+                    })
+                    .ToList()
+            };
+
+            SendImageSet(imageSet);
+
         }
+
+        private static void SendImageSet(ImageSet imageSet)
+        {
+            var multipartContent = new MultipartFormDataContent();
+
+            var imageSetJson = JsonConvert.SerializeObject(imageSet,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+
+            multipartContent.Add(
+                new StringContent(imageSetJson, Encoding.UTF8, "application/json"),
+                "imageset"
+                );
+
+            int counter = 0;
+            foreach (var image in imageSet.Images)
+            {
+                var imageContent = new ByteArrayContent(image.ImageData);
+                imageContent.Headers.ContentType = new MediaTypeHeaderValue(image.MimeType);
+                multipartContent.Add(imageContent, "image" + counter++, image.FileName);
+            }
+
+            var response = new HttpClient()
+                .PostAsync("http://localhost:53908/api/send", multipartContent)
+                .Result;
+
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            Trace.Write(responseContent);
+        }
+
+        //static async void DoShit()
+        //{
+        //    var baseAddress = new Uri(@"http://localhost/CompassDataBroker/api/");
+
+        //    using (var client = new HttpClient())
+        //    {
+        //        var parms = new CreateDocumentParms { DocumentTypeId = (long)DocumentTypes.JeffroesDocType };
+        //        parms.Keywords.Add(CreateKeyword(KeywordTypes.FirstName, "Johnny"));
+        //        parms.Keywords.Add(CreateKeyword(KeywordTypes.LastName, "Northwoods"));
+        //        parms.Keywords.Add(CreateKeyword(KeywordTypes.SSN, "111-11-1111"));
+        //        parms.Keywords.Add(CreateKeyword(KeywordTypes.CompassNumber, "OH123000000001"));
+
+        //        client.BaseAddress = baseAddress;
+        //        var response = await client.PostAsJsonAsync("Document", parms);
+        //    }
+        //}
+
+        //private static Keyword CreateKeyword(KeywordTypes keywordType, string keywordValue)
+        //{
+        //    return new Keyword(new KeywordType((long)keywordType, keywordType.ToString(), typeof(string), ""), keywordValue) { DateTimeValue = DateTime.Now };
+        //}
     }
+
+    public class ImageSet
+    {
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public List<Image> Images { get; set; }
+    }
+
+    public class Image
+    {
+        public string FileName { get; set; }
+
+        public string MimeType { get; set; }
+
+        public byte[] ImageData { get; set; }
+    }
+
 }
