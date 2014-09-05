8fdceb90bf7eff4fba852b6a9f3be755d2bae9c8
JeffroeBodine <jturner@sparkoverflow.com>
2014-09-05 10:15:00 -0400

    more route fixing

--- WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -7,25 +7,36 @@ namespace WebAPI
     {
         public static void Register(HttpConfiguration config)
         {
-         //config.Routes.MapHttpRoute("FileRoute", "api/Document/{documentid}/File", new { controller = "File", action = "Get" });
-         //   config.Routes.MapHttpRoute("KeywordRoute", "api/Document/{id}/Keyword", new { controller = "Keyword", action = "Get" });
             config.Routes.MapHttpRoute("ProgramTypeRoute", "api/Case/{id}/ProgramType", new { controller = "ProgramType", action = "Get" });
-      
-           // config.Routes.MapHttpRoute("ClientRoute", "api/Case/{caseid}/Client", new { controller = "Case", action = "Get" });
 
             config.Routes.MapHttpRoute("CaseClientAddressRoute", "api/Case/{id}/Client/{clientid}/Address", new { controller = "Address", action = "Get" });
             config.Routes.MapHttpRoute("ClientAddressRoute", "api/Client/{clientid}/Address", new { controller = "Address", action = "Get" });
-       
-            //   config.Routes.MapHttpRoute("DocumentRoute", "api/Case/{id}/Client/{clientid}/Document", new { controller = "Document", action = "Get" });
-         //   config.Routes.MapHttpRoute("DocumentMetaDataRoute", "api/Case/{id}/Client/{clientid}/Document/{documentid}/MetaData", new { controller = "DocumentMetaData", action = "Get" });
-         //   config.Routes.MapHttpRoute("FileRoute2", "api/Cases/{id}/Client/{clientid}/Document/{documentid}/File", new { controller = "File", action = "Get" });
-           // config.Routes.MapHttpRoute("AddClientWithCaseroute", "api/{controller}/{id}/Client", new { controller = "Case" });
 
-            config.Routes.MapHttpRoute("CaseRoute", "api/Case/{id}", new {controller = "Case", action = "Get", id = RouteParameter.Optional });
-           
+            config.Routes.MapHttpRoute("CaseRoute", "api/Case/{id}", new { controller = "Case", action = "Get", id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute("ClientRoute", "api/Case/{caseId}/Client/{id}", new { controller = "Client", action = "Get", id = RouteParameter.Optional });
+
+            config.Routes.MapHttpRoute("CaseDocumentRoute", "api/Case/{caseId}/Client/{id}/Document", new { controller = "Document", action = "Get" });
+            config.Routes.MapHttpRoute("ClientDocumentRoute", "api/Client/{id}/Document", new { controller = "Document", action = "Get" });
+
+            config.Routes.MapHttpRoute("CaseDocumentMetaDataRoute", "api/Case/{id}/Client/{clientid}/Document/{documentid}/MetaData", new { controller = "DocumentMetaData", action = "Get" });
+            config.Routes.MapHttpRoute("ClientDocumentMetaDataRoute", "api/Client/{clientid}/Document/{documentid}/MetaData", new { controller = "DocumentMetaData", action = "Get" });
+
+            config.Routes.MapHttpRoute("CaseKeywordRoute", "api/Case/{caseId}/Client/{clientId}/Document/{id}/Keyword", new { controller = "Keyword", action = "Get" });
+            config.Routes.MapHttpRoute("ClientKeywordRoute", "api/Client/{clientId}/Document/{id}/Keyword", new { controller = "Keyword", action = "Get" });
+
+
+
+
             config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional });
             config.Routes.MapHttpRoute("DefaultApiWithAction", "api/{controller}/{id}/{action}", new { id = RouteParameter.Optional });
-          
+
+
+            //  config.Routes.MapHttpRoute("FileRoute", "api/Document/{documentid}/File", new { controller = "File", action = "Get" });
+            //  config.Routes.MapHttpRoute("KeywordRoute", "api/Document/{id}/Keyword", new { controller = "Keyword", action = "Get" });
+            //  config.Routes.MapHttpRoute("DocumentRoute", "api/Case/{id}/Client/{clientid}/Document", new { controller = "Document", action = "Get" });
+            //  config.Routes.MapHttpRoute("FileRoute2", "api/Cases/{id}/Client/{clientid}/Document/{documentid}/File", new { controller = "File", action = "Get" });
+            //  config.Routes.MapHttpRoute("AddClientWithCaseroute", "api/{controller}/{id}/Client", new { controller = "Case" });
+
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
--- WebAPI/Controllers/DocumentController.cs
+++ WebAPI/Controllers/DocumentController.cs
@@ -10,23 +10,14 @@ namespace WebAPI.Controllers
 {
     public class DocumentController : ApiController
     {
-        /// <summary>
-        /// Returns a specific Document given an Id.
-        /// </summary>
-        //public Document Get(string id)
-        //{
-        //    var dms = new OnBase("jturner", "jturner");
-        //    return dms.GetDocumnet(id);
-        //}
-
-        public List<Document> Get(string clientId)
+      public List<Document> Get(string id)
         {
             //Get CompassNumber from Client
 
             string compassNumber;
             using (var rb = new RepositoryBase())
             {
-                compassNumber = rb.Get<Client>(long.Parse(clientId)).CompassNumber;
+                compassNumber = rb.Get<Client>(long.Parse(id)).CompassNumber;
             }
             //Do a document search based on CompassNumber
             var dms = new OnBase("jturner", "jturner");
